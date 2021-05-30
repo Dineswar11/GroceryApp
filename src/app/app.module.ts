c213afb8f3ebfdb6b76f1cabfe2ca4ac03def56f
Dineswar <dineshwarreddy619@gmail.com>
2021-05-30 18:45:49 +0530

    Intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { FruitsVegitablesComponent } from './fruits-vegitables/fruits-vegitables.component';
+import { HomeComponent } from './home/home.component';
+import { SnacksComponent } from './snacks/snacks.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    FruitsVegitablesComponent,
+    HomeComponent,
+    SnacksComponent,
+    ProductCardComponent,
+    CartComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fruits-vegitables/fruits-vegitables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fruits-vegitables',
+  templateUrl: './fruits-vegitables.component.html',
+  styleUrls: ['./fruits-vegitables.component.css']
+})
+export class FruitsVegitablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
