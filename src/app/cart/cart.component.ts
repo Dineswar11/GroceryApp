799981d269dc973e3906cc2819517591afff115b
Dineswar <dineshwarreddy619@gmail.com>
2021-06-01 18:02:51 +0530

    update navbar

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { opacity } from '../animation';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
+  animations:[opacity]
 })
 export class CartComponent implements OnInit {
 
