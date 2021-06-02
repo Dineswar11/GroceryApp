23fc9c77d57568661364056c000d0c7e73eeadac
shruthika <shruthikaakula1920@gmail.com>
2021-06-02 16:15:00 +0530

    2nd,june

--- src/app/Services/fruits-vegitables.service.ts
+++ src/app/Services/fruits-vegitables.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+import {from, Observable } from 'rxjs';
+import{fruitsvegitablesArr} from '../Models/fruits-vegitables.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FruitsVegitablesService {
 
-  constructor() { }
+  constructor(private hc:HttpClient) { }
+
+  getFruitsAndVegitables():Observable<fruitsvegitablesArr[]>{
+    return this.hc.get<fruitsvegitablesArr[]>('http://localhost:3000/fruits_vegitables')
+  }
+  
+
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { opacity } from '../animation';
+import { fruitsvegitablesArr } from '../Models/fruits-vegitables.model';
+import { FruitsVegitablesService } from '../Services/fruits-vegitables.service';
 
 @Component({
   selector: 'app-cart',
@@ -9,12 +12,19 @@ import { opacity } from '../animation';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { 
-    
-  }
+  myProducts:fruitsvegitablesArr[]=[];
 
-
+  constructor(private frObj:FruitsVegitablesService,private router:Router) { }
   ngOnInit(): void {
+    this.frObj.getFruitsAndVegitables().subscribe(
+      products=>{
+        this.myProducts=products;
+        console.log(this.myProducts);
+      },
+      err=>{
+        console.log("error is",err)
+      }
+      )
   }
 
   quantity:number = 1;
