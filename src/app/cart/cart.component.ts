a86d19e7919704d4eb068056ebde35c9eec1b2c8
shruthika <shruthikaakula1920@gmail.com>
2021-06-02 16:34:47 +0530

    changes in fruits-vegitables

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { opacity } from '../animation';
-import { fruitsvegitablesArr } from '../Models/fruits-vegitables.model';
-import { FruitsVegitablesService } from '../Services/fruits-vegitables.service';
+
 
 @Component({
   selector: 'app-cart',
@@ -12,19 +11,11 @@ import { FruitsVegitablesService } from '../Services/fruits-vegitables.service';
 })
 export class CartComponent implements OnInit {
 
-  myProducts:fruitsvegitablesArr[]=[];
+  
 
-  constructor(private frObj:FruitsVegitablesService,private router:Router) { }
+  constructor() { }
   ngOnInit(): void {
-    this.frObj.getFruitsAndVegitables().subscribe(
-      products=>{
-        this.myProducts=products;
-        console.log(this.myProducts);
-      },
-      err=>{
-        console.log("error is",err)
-      }
-      )
+    
   }
 
   quantity:number = 1;
