c8f59fa6ed940153fc835da1ce2def5118c701e9
Dineswar <dineshwarreddy619@gmail.com>
2021-06-15 14:05:51 +0530

    updated cartDS

--- src/app/Services/addtocart.service.ts
+++ src/app/Services/addtocart.service.ts
@@ -7,38 +7,23 @@ export class AddtocartService {
 
   productsAddedToCart: any[] = []
 
-  show: boolean
-
-  count: number
-
   constructor() { }
 
   newProductAddedToCart(newProduct) {
     if (this.productsAddedToCart.length >= 1) {
-      if (this.check(newProduct)) {
+      let ind = this.productsAddedToCart.findIndex(userObj => userObj.id === newProduct.id)
+      if (ind == -1) {
         this.productsAddedToCart.push(newProduct)
         return true
       }
       else return false
     }
     else this.productsAddedToCart.push(newProduct); return true
+    //window.location.reload();
   }
 
   deleteProductFromCart(index) {
     this.productsAddedToCart.splice(index, 1)
-  }
-
-  check(newProduct){
-    for (let p of this.productsAddedToCart) {
-      if (p.name === newProduct.name) {
-        this.count = 1
-        break;
-      }
-      else this.count = 0
-    }
-    if (this.count == 0) return true
-    else return false
-
   }
 
   getTheProductsInCart() {
