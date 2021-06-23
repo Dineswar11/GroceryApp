198e227f6b7aeaf5a4a7eb41a2e38faf017e1a78
Dineswar <dineshwarreddy619@gmail.com>
2021-06-23 12:35:55 +0530

    added authorization interceptor

--- APIs/Middlewares/checkToken.js
+++ APIs/Middlewares/checkToken.js
@@ -3,15 +3,16 @@ const jwt = require('jsonwebtoken')
 
 const checkToken = (req, res, next) => {
     let tokenWithBearer = req.headers.authorization
-    let token = tokenWithBearer.split(" ")[1]
+    let token;
 
     if(token==undefined){
         req.send({message : 'Unauthorazied Access'})
     }
     else {
+        token = tokenWithBearer.split(" ")[1];
         jwt.verify(token,'xyz',(err,decoded)=>{
             if(err){
-                return res.send({message:"Session expired..login to continue..."})
+                return res.send({message:"Invalid/Expired Request .. Login Again to continue"})
             }
             else{
                 next()
